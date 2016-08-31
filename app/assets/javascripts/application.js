26fbad0ceac51df8c188dfb5a3178361c9e4823e
ShmatkoAndrey <shmatuan@gmail.com>
2016-08-31 11:50:02 +0300

    settings proj + install react

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+// require turbolinks
+//= require bootstrap-sprockets
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
---
+++ app/assets/javascripts/components/bodyBox.jsx
@@ -0,0 +1,9 @@
+var BodyBox = new React.createClass({
+    render() {
+        return (
+            <div id = "body-box">
+                <h1> Works! </h1>
+            </div>
+        )
+    }
+});
